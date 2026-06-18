f9bfbca956f1845561099580403db35d49d0b904
Ahmed Sabry <ms6934752@gmail.com>
2026-06-18 17:14:02 +0300

    update the origin url

--- SabryChat.Api/Program.cs
+++ SabryChat.Api/Program.cs
@@ -18,8 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "https://sabry-chat.netlify.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
